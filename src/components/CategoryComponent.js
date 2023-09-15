48de8978d4b0b33031fa18542a38db5d87daa193
PixlPerf <ashwin.shekher@gmail.com>
2023-09-15 13:00:09 +0530

    final Commit

--- src/components/CategoryComponent.js
+++ src/components/CategoryComponent.js
@@ -2,12 +2,17 @@ import React from 'react';
 
 import MenuListComponent from './MenuListComponent.js';
 
+import styled from 'styled-components';
+
+const Header = styled.h1`
+  font-size: '18px';
+  font-weight: '600'; 
+  margin-bottom: '8px';`;
+
 const CategoryComponent = ({ CategoryID, CategoryName, IsCollection }) => {
   return (
     <div style={{ margin: '40px' }}>
-      <div style={{ fontSize: '18px', fontWeight: '600', marginBottom: '8px' }}>
-        {CategoryName}
-      </div>
+      <Header>{CategoryName}</Header>
       <MenuListComponent CategoryID={CategoryID} IsCollection={IsCollection} />
     </div>
   );
